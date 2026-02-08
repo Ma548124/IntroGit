82605cdf9609ed227b397d5eeadd653a02d36ce0
CIS106 Angel Munoz <ma54812@mail.harpercollege.edu>
2026-02-08 17:05:33 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,75 +1,162 @@
 //
 //  main.cpp
-//  pointers lab
+//  I Get The Point!
 //
-//  Created by Angel Munoz on 1/27/26.
+//  Created by Angel Munoz on 2/3/26.
 //
 
 #include <iostream>
-#include<cstdlib>
-#include<ctime>
-#include<string>
+#include <cmath>
+#include <sstream>
 
 using namespace std;
 
+class Point {
+private:
+    double x, y;
 
-int main() {
-    
-    int a , b;
-    cout<<"Please use or type two integers:";
-    cin>>a>>b;
-    
-    int* ptr_a = &a;
-    int* ptr_b = &b;
-    
-   
-    
-    cout<<"Dereferenced ptr_a:"<<endl;
-    cout<<"Dereferenced ptr_b:"<<endl;
-    
-    //using the second program fidng the max array in pointer
-    srand(time(0));
-    int arr[10];
-    for(int i = 0 ; i < 10; i++){
-        arr[i] = rand() % 100;
+public:
+    // Constructor
+    Point(double x = 0, double y = 0) : x(x), y(y) {}
+
+    // Getters (optional, for testing)
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    // Overloaded operators
+
+    // Distance: p1 - p2 returns distance
+    friend double operator-(const Point& p1, const Point& p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return std::sqrt(dx * dx + dy * dy);
     }
-    int* max_ptr = arr;
-    for (int* ptr = arr + 1; ptr < arr + 10; ptr++) {
-        if(*ptr > *max_ptr){
-            max_ptr = ptr;
+
+    // Equality: p1 == p2
+    friend bool operator==(const Point& p1, const Point& p2) {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+
+    // Inequality: p1 != p2
+    friend bool operator!=(const Point& p1, const Point& p2) {
+        return !(p1 == p2);
+    }
+
+    // Midpoint: p1 / p2 returns midpoint
+    friend Point operator/(const Point& p1, const Point& p2) {
+        return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+    }
+
+    // Output streaming: cout << p
+    friend ostream& operator<<(ostream& os, const Point& p) {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
+    }
+
+    // Input streaming: cin >> p, expects (x, y)
+    friend istream& operator>>(istream& is, Point& p) {
+        char ch;
+        is >> ch; // Read '('
+        if (ch != '(') {
+            is.setstate(std::ios::failbit);
+            return is;
         }
+        is >> p.x;
+        is >> ch; // Read ','
+        if (ch != ',') {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        is >> p.y;
+        is >> ch; // Read ')'
+        if (ch != ')') {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+        return is;
     }
-    cout<<"The maximum value in the array is: "<<*max_ptr<<endl;
-    
-    char str[] = "Hello World!";
-    char* ptr = str;
-    int length = 0;
-    while(*ptr !=  '\0'){
-        length++;
-        ptr++;
+
+    // Prefix ++: ++p increments x
+    Point& operator++() {
+        ++x;
+        return *this;
     }
-    cout<<" The length of the string is:"<< length<<endl;
-    
-    char str2[] = "Hello";
-    char* ptr2 = str2;
-    while(ptr2 >= str2){
-        cout<< *ptr2;
-        ptr2--;
-        
+
+    // Postfix ++: p++ increments y
+    Point operator++(int) {
+        Point temp = *this;
+        ++y;
+        return temp;
     }
-    cout<<endl;
-    
-    vector<int> v = {1,2,3,4,5,6};
-    auto start = v.begin();
-    auto end = v.end() - 1;
-    while(start<end){
-        start++;
-        end--;
+
+    // Prefix --: --p decrements x
+    Point& operator--() {
+        --x;
+        return *this;
     }
-    if(start == end){
-        cout<<"The midpoint value is:"<<*start<<endl;
-    }else{
-        cout<<"The midpoint value of the (smaller index) is:"<<*(end+1)<<endl;
+
+    // Postfix --: p-- decrements y
+    Point operator--(int) {
+        Point temp = *this;
+        --y;
+        return temp;
     }
+
+    // Subscript: p[0] for x, p[1] for y
+    double& operator[](int index) {
+        if (index == 0) return x;
+        if (index == 1) return y;
+        throw out_of_range("Index must be 0 or 1");
+    }
+
+    const double& operator[](int index) const {
+        if (index == 0) return x;
+        if (index == 1) return y;
+        throw out_of_range("Index must be 0 or 1");
+    }
+};
+
+// Example usage (for testing)
+int main() {
+    Point p1(5.16, 13.567);
+    Point p2(2.98, 8.24);
+
+    // Distance
+    cout << "Distance: " << p1 - p2 << endl;
+
+    // Equality
+    cout << "Equal: " << (p1 == p2) << endl;
+    cout << "Not equal: " << (p1 != p2) << endl;
+
+    // Midpoint
+    Point mid = p1 / p2;
+    cout << "Midpoint: " << mid << endl;
+
+    // Output
+    cout << "p1: " << p1 << endl;
+    cout << "p2: " << p2 << endl;
+
+    // Increment/Decrement
+    ++p1; // x++
+    cout << "After ++p1: " << p1 << endl;
+    p1++; // y++
+    cout << "After p1++: " << p1 << endl;
+    --p1; // x--
+    cout << "After --p1: " << p1 << endl;
+    p1--; // y--
+    cout << "After p1--: " << p1 << endl;
+
+    // Subscript
+    cout << "p1[0]: " << p1[0] << endl;
+    cout << "p1[1]: " << p1[1] << endl;
+    p1[0] = 10.0;
+    cout << "After p1[0] = 10: " << p1 << endl;
+
+    // Input
+    Point p3;
+    cout << "Enter a point in the form that will be (x, y): ";
+    cin >> p3;
+    cout << "You entered this equation: " << p3 << endl;
+
     return 0;
 }
